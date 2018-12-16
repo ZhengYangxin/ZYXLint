e86028a10b10460488488328d2be203e4a4a45b1
zhengyangxin <ZhengSunstar@gmail.com>
2018-12-16 22:12:20 +0800

    da

--- app/src/main/java/com/zyx/app/zyxlint/MainActivity.java
+++ app/src/main/java/com/zyx/app/zyxlint/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                //dadasdasdasddadsa
+                //dadasdasdasddadsdasdasa
             }
         }).run();
     }
